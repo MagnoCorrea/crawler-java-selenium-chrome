483dddaed6f7f1d5c60d9458ba678ee4560d4f7a
magno <magno@ARKANJO4>
2019-09-27 21:59:30 -0300

    Initial Version

---
+++ README.md
@@ -0,0 +1,8 @@
+# crawler-java-selenium-chrome
+A simple Chrome Selenium Web Crawler project in Java with Eclipse
+
+## Instalation
+```
+git clone https://github.com/MagnoCorrea/crawler-java-selenium-chrome.git
+```
+
---
+++ src/main/java/com/magnocorrea/crawler_java_selenium_chrome/sandbox/Main.java
@@ -0,0 +1,14 @@
+package com.magnocorrea.crawler_java_selenium_chrome.sandbox;
+/**
+ * A simple main to be an great sandbox! :)
+ * @author magno@magnocorrea.com
+ *
+ */
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println("Hello Wold!!!");
+
+	}
+
+}
